fc5a159cbd328d32fd21ca4b0a4e90509aa5ae7d
James Montalvo <edwin.j.montalvo@nasa.gov>
2015-02-02 18:50:40 -0600

    Convert deprecated msg formats; code format cleanup

--- HeaderFooter.php
+++ HeaderFooter.php
@@ -3,17 +3,16 @@
  * @author Jean-Lou Dupont
  * @author Jamesmontalvo3
  * @package HeaderFooter
- * @version 2.1.0
- * @Id $Id: HeaderFooter.php 821 2008-01-04 02:36:30Z jeanlou.dupont $
+ * @version 2.1.1
  */
 
 # Credits
 $GLOBALS['wgExtensionCredits']['other'][] = array( 
-	'name'		=> 'HeaderFooter', 
-	'version'	 => '2.1.0',
+	'name'		  => 'HeaderFooter', 
+	'version'	  => '2.1.1',
 	'author'	  => 'Jean-Lou Dupont, James Montalvo, Douglas Mason', 
 	'description' => 'Enables per-page/per-namespace headers and footers',
-	'url' 		=> 'http://mediawiki.org/wiki/Extension:HeaderFooter',			
+	'url' 		  => 'http://mediawiki.org/wiki/Extension:HeaderFooter',			
 );
 
 # Hooks
