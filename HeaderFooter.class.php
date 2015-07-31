28687ee302b14f86727a1cead33aa8ff96e36ece
Stefan <sm8ps-1@yahoo.com>
2015-07-31 17:37:22 +0000

    Per category headers/footers added

--- HeaderFooter.class.php
+++ HeaderFooter.class.php
@@ -19,6 +19,9 @@ class HeaderFooter
 		$ns = $wgTitle->getNsText();
 		$name = $wgTitle->getPrefixedDBKey();
 
+		$categories = $wgTitle->getParentCategories();
+		$categories = array_keys( $categories) ;
+
 		$text = $parserOutput->getText();
 
 		$nsheader = "hf-nsheader-$ns";
@@ -29,6 +32,22 @@ class HeaderFooter
 
 		$text = '<div class="hf-header">'.self::conditionalInclude( $text, '__NOHEADER__', $header ).'</div>'.$text;
 		$text = '<div class="hf-nsheader">'.self::conditionalInclude( $text, '__NONSHEADER__', $nsheader ).'</div>'.$text;
+
+		/*
+		* headers/footers pages: hf-cat{header,footer}-<NAME OF CATEGORY>
+		* new token __NOCATHEADER__
+		* using div class="hf-cat{header,footer}"
+		*/
+		$NS14 = "Kategorie" ;	// must be internationalized but don't know how
+					// need value of nstab-category in i18n-files
+		$once = 1 ;		// cannot pass constant 1 to str_replace()
+		foreach( $categories as &$category ) {
+	        $cat = str_replace( $NS14.":", "", $category, $once) ;
+	        $catheader = "hf-catheader-$cat" ;
+	        $text = '<div class="hf-catheader">'.self::conditionalInclude( $text, '__NOCATHEADER__', $catheader ).'</div>'.$text;
+        	$catfooter = "hf-catfooter-$cat" ;
+        	$text .= '<div class="hf-catfooter">'.self::conditionalInclude( $text, '__NOCATFOOTER__', $catfooter ).'</div>';
+}
 
 		$text .= '<div class="hf-footer">'.self::conditionalInclude( $text, '__NOFOOTER__', $footer ).'</div>';
 		$text .= '<div class="hf-nsfooter">'.self::conditionalInclude( $text, '__NONSFOOTER__', $nsfooter ).'</div>';
